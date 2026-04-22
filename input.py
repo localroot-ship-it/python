e1a761dfd71fad38da4941e216656134d6ff4fe1
naveed <lone54012@gmail.com>
2026-04-22 10:42:45 +0530

    Uncommented user input and traffic light logic

--- input.py
+++ input.py
@@ -1,18 +1,9 @@
 #taking input from user through keyboard 
-# name= input("name :")
-# age= int(input("age :"))
-# price= float(input("price:"))
-# print("my name is ",name,"my age is",age,"and my price is ",price)
-# name= input("name:")
-# print("welcome",name)
-#traffic light checking
-light=input("light color :")
-if(light=="red"):
-    print("stop")
-if(light=="yellow"):
-    print("look around")
-if(light=="green"):
-    print("go go ...")
-else:
-    print("light is broken")
+name= input("name :")
+ age= int(input("age :"))
+ price= float(input("price:"))
+ print("my name is ",name,"my age is",age,"and my price is ",price)
+ name= input("name:")
+ print("welcome",name)
+
 
