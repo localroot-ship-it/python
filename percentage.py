a1be0405ec343472111bc80718ccf3ff9cc2706f
naveed <lone54012@gmail.com>
2026-04-22 11:56:22 +0530

    Add files via upload

---
+++ percentage.py
@@ -0,0 +1,8 @@
+class student:
+    def __init__(self,phy,chem,math):
+        self.phy=phy
+        self.chem=chem
+        self.math=math
+        self.percentage=str((self.phy+self.chem+self.math /3))+"%"
+stu1=student(38,39,39)
+print(stu1.percentage)
