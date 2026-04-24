1b1beebe64ff3308d07390ef972f5e3a41f28316
naveed <lone54012@gmail.com>
2026-04-24 11:58:36 +0530

    Add files via upload

---
+++ practice.py/lecture_eight.py/miniproject.py
@@ -0,0 +1,27 @@
+# this is number guessing game.
+import random
+
+target=random.randint(1,100)
+while True:
+    userChoice=input("guess the target or quit (q) :")
+    if(userChoice=="q"):
+        break
+    userChoice=int(userChoice)
+    if(userChoice==target):
+        print("success : correct guess!!")
+        break
+    elif(userChoice<target):
+        print("your number is too small.take bigger guess... ")
+    else:
+        print("your number is too bigger.Take small guess...")
+print("______game over_____")
+
+# random passworord generartor
+import random
+import string
+pass_len=10
+charValues=string.ascii_letters + string.digits + string.punctuation
+password=""
+for i in range(pass_len):
+    password+=random.choice(charValues)
+print("your random password is ",password)
